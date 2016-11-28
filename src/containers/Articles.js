19bc8a1a145072aff76003b2e40c497ebd7c46ec
OperKH <oper.kh@gmail.com>
2016-11-28 20:48:18 +0200

    Add PropTypes

--- src/containers/Articles.js
+++ src/containers/Articles.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes  } from 'react';
 import { connect } from 'react-redux';
 import ArtilceList from '../components/ArticleList';
 
@@ -6,6 +6,10 @@ import ArtilceList from '../components/ArticleList';
 	({articles}) => ({articles})
 )
 class Article extends Component {
+	static propTypes = {
+		articles: PropTypes.array.isRequired
+	}
+
 	render() {
 		const { articles } = this.props;
 		return <ArtilceList articles={ articles } />;
