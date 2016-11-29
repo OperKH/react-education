b775fcd9c335fbb0bdb280ae060ce4cd1a169329
OperKH <oper.kh@gmail.com>
2016-11-29 21:13:53 +0200

    Move Filers code from ArticleList to Filters container.

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,74 +1,20 @@
 import React, { Component, PropTypes } from 'react';
 import Article from './Article';
-
-import Select from 'react-select';
-import 'react-select/dist/react-select.css';
-
-import DayPicker, { DateUtils } from "react-day-picker";
-import '../css/DayPicker.css';
-
-const dateOptions = {day: 'numeric', month: 'long', year: 'numeric'};
+import Filters from '../containers/Filters';
 
 class ArticleList extends Component {
 	static propTypes = {
 		articles: PropTypes.array.isRequired
 	}
 
-	state = {
-		selectedArticles: null,
-		selectedDays: DateUtils.addDayToRange(new Date('2016-01-05'), {	to: new Date('2016-01-07')})
-	};
-
-	handleSelectchange = selectedArticles => {
-		this.setState({
-			selectedArticles
-		});
-	}
-
-	handleDayClick = (e, day) => {
-		const { selectedDays } = this.state;
-		const range = DateUtils.addDayToRange(day, selectedDays);
-		this.setState({
-            selectedDays: range
-        });
-    }
-
 	render() {
 		const { articles } = this.props;
 		const articleList = articles.map(article => <Article article = { article } key = { article.id } />);
 
-		const selectOptions = articles.map(article => ({
-			label: article.title,
-			value: article.id
-		}));
-
-		const { selectedArticles, selectedDays } = this.state;
-		const { from, to } = selectedDays;
-
 		return (
 			<section>
 				<h2 className="articles-header">Articles:</h2>
-				<div className="article-list-time-range">
-					<span>С </span>
-					<time dateTime={ from ? from.toISOString() : ''}>{ from ? from.toLocaleString('ru-RU', dateOptions) : ''}</time>
-					<span> по </span>
-					<time dateTime={ to ? to.toISOString() : '' }>{ to ? to.toLocaleString('ru-RU', dateOptions) : '' }</time>
-				</div>
-				<Select
-					options={selectOptions}
-					value={selectedArticles}
-					onChange={this.handleSelectchange}
-					multi={true}
-				/>
-				<DayPicker
-					numberOfMonths={3}
-					firstDayOfWeek={1}
-					initialMonth={ selectedDays.from }
-					selectedDays={day => DateUtils.isDayInRange(day, selectedDays)}
-					onDayClick={this.handleDayClick}
-					weekdaysShort={ ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'] }
-					months={ ['Январь', 'Февраль', 'Март', 'Апрель', 'Мая', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'] }
-				/>
+				<Filters />
 				<ol className="article-list">
 					{ articleList }
 				</ol>
---
+++ src/containers/Filters.js
@@ -0,0 +1,77 @@
+import React, { Component, PropTypes} from 'react';
+import { connect } from 'react-redux';
+
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+
+import DayPicker, { DateUtils } from "react-day-picker";
+import '../css/DayPicker.css';
+
+const dateOptions = {day: 'numeric', month: 'long', year: 'numeric'};
+
+@connect (
+	({articles}) => ({articles})
+)
+class Filters extends Component {
+	static propTypes = {
+		articles: PropTypes.array.isRequired
+	}
+
+	state = {
+		selectedArticles: null,
+		selectedDays: DateUtils.addDayToRange(new Date('2016-01-05'), {	to: new Date('2016-01-07')})
+	};
+
+	handleSelectchange = selectedArticles => {
+		this.setState({
+			selectedArticles
+		});
+	}
+
+	handleDayClick = (e, day) => {
+		const { selectedDays } = this.state;
+		const range = DateUtils.addDayToRange(day, selectedDays);
+		this.setState({
+            selectedDays: range
+        });
+    }
+
+	render() {
+		const { articles } = this.props;
+		const selectOptions = articles.map(article => ({
+			label: article.title,
+			value: article.id
+		}));
+
+		const { selectedArticles, selectedDays } = this.state;
+		const { from, to } = selectedDays;
+
+		return (
+			<div>
+				<Select
+					options={selectOptions}
+					value={selectedArticles}
+					onChange={this.handleSelectchange}
+					multi={true}
+				/>
+				<div className="article-list-time-range">
+					<span>С </span>
+					<time dateTime={ from ? from.toISOString() : ''}>{ from ? from.toLocaleString('ru-RU', dateOptions) : ''}</time>
+					<span> по </span>
+					<time dateTime={ to ? to.toISOString() : '' }>{ to ? to.toLocaleString('ru-RU', dateOptions) : '' }</time>
+				</div>
+				<DayPicker
+					numberOfMonths={3}
+					firstDayOfWeek={1}
+					initialMonth={ selectedDays.from }
+					selectedDays={day => DateUtils.isDayInRange(day, selectedDays)}
+					onDayClick={this.handleDayClick}
+					weekdaysShort={ ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'] }
+					months={ ['Январь', 'Февраль', 'Март', 'Апрель', 'Мая', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'] }
+				/>
+			</div>
+		);
+	}
+}
+
+export default Filters;
